276579dba9a798fa3a172be7d86f82e963e76cba
tompiler <tomlynch3091341@googlemail.com>
2018-07-28 17:34:47 +0100

    Added zoom functionality to both x (banded) and y (continuous) axis.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ function drawChart() {
 
 	d3.csv("FTSE.csv").then(function(prices) {
 
+		const candleWidth = 5
+
 		for (var i = 0; i < prices.length; i++) {
 			var dateFormat = d3.timeParse("%Y-%m-%d");
 			prices[i]['Date'] = dateFormat(prices[i]['Date'])
@@ -21,20 +23,17 @@ function drawChart() {
 					.attr("transform", "translate(" +margin.left+ "," +margin.top+ ")");
 		
 		// Clip path to prevent shapes 'leaking' outside chart body
-/* 		svg.append("defs")
+		svg.append("defs")
 		   .append("clipPath")
 			.attr("id", "clip")
-			.attr("transform", "translate("+-100+",0)")
 		   .append("rect")
 		    .attr("width", w)
 			.attr("height", h)
-			.attr("transform", "translate("+-100+",0)"); */
-		
-		var zoom = d3.zoom()
-					 .scaleExtent([0.75, 15000])
-					 .translateExtent([[-10000, -10000], [10000, 10000]])
-					 .on("zoom", zoomed);
-		
+
+		var chartBody = svg.append("g")
+					.attr("class", "chartBody")
+					.attr("clip-path", "url(#clip)");
+
 		// x axis
 		var xmin = d3.min(prices.map(function(r){ return r.Date.getTime(); }));
 		var xmax = d3.max(prices.map(function(r){ return r.Date.getTime(); }));
@@ -46,8 +45,9 @@ function drawChart() {
 					   .tickFormat(function(d, i){ if (i % 14 == 0) {return d.getDate() + ' ' + getShortMonth(d)}});
 
 		var gX = svg.append("g")
-		   			.attr("class", "axis") //Assign "axis" class
-		   			.attr("transform", "translate(0," + h + ")")
+		   			.attr("class", "x axis") //Assign "axis" class
+					   .attr("transform", "translate(0," + h + ")")
+					   .attr("clip-path", "url(#clip)")
 		   			.call(xAxis);
 		
 		// y axis
@@ -59,44 +59,71 @@ function drawChart() {
 				      .ticks(Math.round((ymax - ymin)/100))
 		
 		var gY = svg.append("g")
-		   			.attr("class", "axis")
+		   			.attr("class", "y axis")
 		   			.call(yAxis);
 		
 		// draw rectangles
-		svg.selectAll("rect")
+		chartBody.selectAll("rect")
 		   .data(prices)
 		   .enter()
 		   .append("rect")
 		   .attr("x", function(d){
 				return xScale(d.Date);
 		   })
+		   .attr("class", "candle")
 		   .attr("y", function(d){return yScale(Math.max(d.Open, d.Close));})
-		   .attr("width", function(d){return 0.5*w/prices.length;})
+		   .attr("width", candleWidth)
 		   .attr("height", function(d){return yScale(Math.min(d.Open, d.Close))-yScale(Math.max(d.Open, d.Close	));})
 		   .attr("fill", function(d){return d.Open>d.Close ? "red" : "green"});
 		
 		// draw high and low
-		svg.selectAll("g.line")
+		chartBody.selectAll("g.line")
 		   .data(prices)
 		   .enter()
 		   .append("line")
 		   .attr("class", "stem")
-		   .attr("x1", function(d){ return xScale(d.Date) + 0.25*w/prices.length; })
-		   .attr("x2", function(d){ return xScale(d.Date) + 0.25*w/prices.length; })
+		   .attr("x1", function(d){return xScale(d.Date) + 0.5*candleWidth; })
+		   .attr("x2", function(d){return xScale(d.Date) + 0.5*candleWidth; })
 		   .attr("y1", function(d){return yScale(d.High);})
 		   .attr("y2", function(d){return yScale(d.Low);})
 		   .attr("stroke", function(d){return d.Open > d.Close ? "red" : "green"; })
 
-		function zoomed() {
+		svg.append("rect")
+		   .attr("id","rect")
+		   .attr("width", w)
+		   .attr("height", h)
+		   .style("fill", "none")
+		   .style("pointer-events", "all")
+		
+		d3.select("#rect").call(zoom);
 
-			gX.call(xAxis.scale(d3.event.transform.rescaleX(xScale)));
-			gY.call(yAxis.scale(d3.event.transform.rescaleY(yScale)));
-			var t = d3.event.transform, xt = t.rescaleX(xScale), yt = t.rescaleY(yScale)
-			//svg.select(".line")
-			//   .attr("d",priceSeries.x(function(d) { return xt(d.dt);})
-			//							 .y(function(d) { return yt(d.price);}));
-		};
-			  
+		function zoom(svg) {
+			const extent = [[margin.left, margin.top], [w - margin.right, h - margin.top]];
+			console.log("Zoom")
+			
+			d3.select("#rect").call(d3.zoom()
+				.scaleExtent([1, 8])
+				.translateExtent(extent)
+				.extent(extent)
+				.on("zoom", zoomed));
+			
+			function zoomed() {
+			  var t = d3.event.transform, yt = t.rescaleY(yScale)
+
+			  xScale.range([0, w].map(d => d3.event.transform.applyX(d)));
+			  chartBody.selectAll(".candle").attr("x", d => xScale(d.Date)).attr("width", xScale.bandwidth());
+			  chartBody.selectAll(".stem").attr("x1", d => xScale(d.Date) + xScale.bandwidth()/2);
+			  chartBody.selectAll(".stem").attr("x2", d => xScale(d.Date) + xScale.bandwidth()/2);
+			  gX.call(xAxis)
+
+			  chartBody.selectAll(".candle").attr("y", d => yt(Math.max(d.Open, d.Close)));
+			  chartBody.selectAll(".candle").attr("height", d => yt(Math.min(d.Open, d.Close))-yt(Math.max(d.Open, d.Close)));
+			  chartBody.selectAll(".stem").attr("y1", d => yt(d.High));
+			  chartBody.selectAll(".stem").attr("y2", d => yt(d.Low));
+			  gY.call(yAxis.scale(d3.event.transform.rescaleY(yScale)));
+
+			}
+		}
 
 	});
 }
