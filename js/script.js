92946561cfe0216048a20e36b906d74f4d472f9e
tompiler <tomlynch3091341@googlemail.com>
2018-08-05 11:39:36 +0100

    Several major changes. Static y-axis. Implemented dual linear & banded x-axis scales to preserve tick labels and candle positions on zoom perfectly. Added wordwrapping to x-axis labels + formatting. Added small white rects for days where the close price equals the open price.

--- js/script.js
+++ js/script.js
@@ -1,235 +1,160 @@
-
-// Draw a candlestick chart based on passed prices
-function drawChart(frequency) {
+function drawChart() {
 
 	d3.csv("UKX_5Mins_20180709_20180716.csv").then(function(prices) {
-
-		const candleWidth = 2
+		
 		const months = {0 : 'Jan', 1 : 'Feb', 2 : 'Mar', 3 : 'Apr', 4 : 'May', 5 : 'Jun', 6 : 'Jul', 7 : 'Aug', 8 : 'Sep', 9 : 'Oct', 10 : 'Nov', 11 : 'Dec'}
+		const candleWidth = 5
 
 		var dateFormat = d3.timeParse("%Y-%m-%d %H:%M");
 		for (var i = 0; i < prices.length; i++) {
-			//var dateFormat = d3.timeParse("%Y-%m-%d");
 			
 			prices[i]['Date'] = dateFormat(prices[i]['Date'])
 		}
 
 		const margin = {top: 15, right: 65, bottom: 205, left: 50},
 		w = 1190 - margin.left - margin.right,
-		h = 870 - margin.top - margin.bottom;
+		h = 770 - margin.top - margin.bottom;
+
+		var svg = d3.select("#container")
+						.attr("width", w + margin.left + margin.right)
+						.attr("height", h + margin.top + margin.bottom)
+						.append("g")
+						.attr("transform", "translate(" +margin.left+ "," +margin.top+ ")");
+
+		let dates = _.map(prices, 'Date');
 		
-		var svg = d3.select("#candleChart")
-					.append("svg")
-					.attr("width", w + margin.left + margin.right)
-					.attr("height", h + margin.top + margin.bottom)
-					.append("g")
-					.attr("transform", "translate(" +margin.left+ "," +margin.top+ ")");
-		
-
-		// Clip path to prevent shapes 'leaking' outside chart body
-		svg.append("defs")
-		   .append("clipPath")
-			.attr("id", "clip")
-		   .append("rect")
-		    .attr("width", w)
-			.attr("height", h)
-
-		var chartBody = svg.append("g")
-					.attr("class", "chartBody")
-					.attr("clip-path", "url(#clip)");
-
-		// x axis
 		var xmin = d3.min(prices.map(function(r){ return r.Date.getTime(); }));
 		var xmax = d3.max(prices.map(function(r){ return r.Date.getTime(); }));
-		var xScale = d3.scaleBand().domain(_.map(prices, 'Date'))
-					   .range([0, w])
-					   .padding(0.2)
-
-		//var handle = dateRange()
-		//var filtered = _.filter(prices, d => d.Date >= handle['left'] && d.Date <= handle['right'])
-
-		/* var xAxis = d3.axisBottom()
-					   .scale(xScale)
-					   .tickFormat(xScale.domain().filter(function(d,i){ 
-							return !(i%100)
-						}))
-					   //.tickFormat(d => d.toDateString() + ' ' + d.toTimeString())
-					   .tickSize(2); */
-		
-		//var t = d3.event.transform;
-		
+		var xScale = d3.scaleLinear().domain([-1, dates.length])
+						.range([0, w])
+		let xBand = d3.scaleBand().domain(d3.range(-1, dates.length)).range([0, w]).padding(0.3)
 		var xAxis = d3.axisBottom()
-					   .scale(xScale)
-					   .tickValues(xScale.domain().filter(function(d,i){
-						//console.log(d3.zoomTransform(d3.select('#rect')))
-						return !(i%50)
-					}))
-					.tickFormat(function(d){
+					  .scale(xScale)
+					    .tickFormat(function(d, e){
+						d = dates[d]
 						hours = d.getHours()
 						minutes = (d.getMinutes()<10?'0':'') + d.getMinutes() 
 						amPM = hours < 13 ? 'am' : 'pm'
-						 return hours + ':' + minutes + amPM + ' ' + d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear()}
-					);
-
-
-		var gX = svg.append("g")
-		   			.attr("class", "axis x-axis") //Assign "axis" class
-					   .attr("transform", "translate(0," + h + ")")
-					   .attr("clip-path", "url(#clip)")
-					   .call(xAxis)
-
-		gX.selectAll(".tick text")
-		  .attr("transform", "rotate(270) translate(-70, 0)")
-		  //.select('text')
-		  //.call(wrap, xScale.bandwidth());
+						return hours + ':' + minutes + amPM + ' ' + d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear()
+						});
 		
-		// y axis
-		var ymin = d3.min(prices.map(function(r){return r.Low;}));
-		var ymax = d3.max(prices.map(function(r){return r.High;}));
+		svg.append("rect")
+					.attr("id","rect")
+					.attr("width", w)
+					.attr("height", h)
+					.style("fill", "none")
+					.style("pointer-events", "all")
+					.attr("clip-path", "url(#clip)")
+		
+		var gX = svg.append("g")
+					.attr("class", "axis x-axis") //Assign "axis" class
+					.attr("transform", "translate(0," + h + ")")
+					.call(xAxis)
+		
+		gX.selectAll(".tick text")
+		  .call(wrap, xBand.bandwidth())
+
+		var ymin = d3.min(prices.map(r => r.Low));
+		var ymax = d3.max(prices.map(r => r.High));
 		var yScale = d3.scaleLinear().domain([ymin, ymax]).range([h, 0]).nice();
 		var yAxis = d3.axisLeft()
 					  .scale(yScale)
-				      .ticks(10)
 		
 		var gY = svg.append("g")
-		   			.attr("class", "axis y-axis")
-		   			.call(yAxis);
+					.attr("class", "axis y-axis")
+					.call(yAxis);
+		
+		var chartBody = svg.append("g")
+					.attr("class", "chartBody")
+					.attr("clip-path", "url(#clip)");
 		
 		// draw rectangles
-		chartBody.selectAll("rect")
+		let candles = chartBody.selectAll(".candle")
 		   .data(prices)
 		   .enter()
 		   .append("rect")
-		   .attr("x", function(d){
-				return xScale(d.Date);
-		   })
+		   .attr('x', (d, i) => xScale(i) - xBand.bandwidth())
 		   .attr("class", "candle")
-		   .attr("y", function(d){return yScale(Math.max(d.Open, d.Close));})
-		   .attr("width", candleWidth)
-		   .attr("height", function(d){return yScale(Math.min(d.Open, d.Close))-yScale(Math.max(d.Open, d.Close	));})
-		   .attr("fill", function(d){return d.Open>d.Close ? "red" : "green"});
+		   .attr('y', d => yScale(Math.max(d.Open, d.Close)))
+		   .attr('width', xBand.bandwidth())
+		   .attr('height', d => (d.Open === d.Close) ? 1 : yScale(Math.min(d.Open, d.Close))-yScale(Math.max(d.Open, d.Close)))
+		   .attr("fill", d => (d.Open === d.Close) ? "silver" : (d.Open > d.Close) ? "red" : "green")
 		
 		// draw high and low
-		chartBody.selectAll("g.line")
+		let stems = chartBody.selectAll("g.line")
 		   .data(prices)
 		   .enter()
 		   .append("line")
 		   .attr("class", "stem")
-		   .attr("x1", function(d){return xScale(d.Date) + 0.5*candleWidth; })
-		   .attr("x2", function(d){return xScale(d.Date) + 0.5*candleWidth; })
-		   .attr("y1", function(d){return yScale(d.High);})
-		   .attr("y2", function(d){return yScale(d.Low);})
-		   .attr("stroke", function(d){return d.Open > d.Close ? "red" : "green"; })
+		   .attr("x1", (d, i) => xScale(i) - xBand.bandwidth()/2)
+		   .attr("x2", (d, i) => xScale(i) - xBand.bandwidth()/2)
+		   .attr("y1", d => yScale(d.High))
+		   .attr("y2", d => yScale(d.Low))
+		   .attr("stroke", d => (d.Open === d.Close) ? "white" : (d.Open > d.Close) ? "red" : "green");
+		
+		svg.append("defs")
+		.append("clipPath")
+			.attr("id", "clip")
+		.append("rect")
+			.attr("width", w)
+			.attr("height", h)
 
-		svg.append("rect")
-		   .attr("id","rect")
-		   .attr("width", w)
-		   .attr("height", h)
-		   .style("fill", "none")
-		   .style("pointer-events", "all")
-		
-		const extent = [[margin.left, margin.top], [w - margin.right, h - margin.top]];
-		
+		const extent = [[0, 0], [w, h]];
+
 		var zoom = d3.zoom()
-					 .scaleExtent([1, 8])
-					 .translateExtent(extent)
-					 .extent(extent)
-					 .on("start", console.log("Its started!"))
-					 .on("zoom", zoomed)
-					 .on("end", console.log("Its ended!"))
-
-		d3.select("#rect").call(zoom);
-
-		console.log(d3.zoomTransform(d3.select("#rect").call(zoom)).k, prices.length);
+		  .scaleExtent([1, 8])
+		  .translateExtent(extent)
+		  .extent(extent)
+		  .on("zoom", zoomed)
 		
-		function dateRange(t){
-			var t = d3.event.transform
-			var invDomain = xScale.domain()
-			var invRange = xScale.range()
-			var invScale = d3.scaleQuantize().domain(invRange).range(invDomain)
-
-			leftDate = invScale(Math.abs(xScale.range()[0])/t.k)
-			rightDate = invScale(w-Math.abs(w-xScale.range()[1])/t.k)
-
-			return {'left' : leftDate, 'right' : rightDate};
-		}
-
+		svg.call(zoom);
+		
 		function zoomed() {
-			var t = d3.event.transform, yt = t.rescaleY(yScale)
-			xScale.range([0, w].map(d => d3.event.transform.applyX(d)));
-			var handle = dateRange();
-			var currentZoom = d3.event.transform;
-			console.log(d3.zoomTransform(zoom).k, prices.length);
-			filtered = _.filter(prices, d => d.Date >= leftDate && d.Date <= rightDate)
-			minp = d3.min(filtered, d => d.Low || Infinity)
-			maxp = d3.max(filtered, d => d.High)
+			if (d3.event.transform.k < 1) {
+				d3.event.transform.k = 1
+				return
+			  }
+			var t = d3.event.transform;
 
-			chartBody.selectAll(".candle").attr("x", d => xScale(d.Date)).attr("width", xScale.bandwidth());
-			chartBody.selectAll(".stem").attr("x1", d => xScale(d.Date) + xScale.bandwidth()/2);
-			chartBody.selectAll(".stem").attr("x2", d => xScale(d.Date) + xScale.bandwidth()/2);
+			gX.call(
+				d3.axisBottom(d3.event.transform.rescaleX(xScale)).tickFormat((d, e, target) => {
+				 if (d >= 0 && d <= dates.length-1) {
+					d = dates[d]
+					
+					hours = d.getHours()
+					minutes = (d.getMinutes()<10?'0':'') + d.getMinutes() 
+					amPM = hours < 13 ? 'am' : 'pm'
+					return hours + ':' + minutes + amPM + ' ' + d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear()
+				 }
+				})
+			  )
 
-			//console.log(handle['left'], handle['right']);
+			hideTicksWithoutLabel()
 			
-			var xmin = d3.min(filtered.map(function(r){ return r.Date.getTime(); }));
-			var xmax = d3.max(filtered.map(function(r){ return r.Date.getTime(); }));
-			var xScaleZ = d3.scaleBand().domain(_.map(filtered, 'Date'))
-						   .range([0, w])
-						   .padding(0.2)
+			candles.attr("x", (d, i) => d3.event.transform.rescaleX(xScale)(i) - (xBand.bandwidth()*d3.event.transform.k)/2)
+				   //.attr("y", (d) => d3.event.transform.rescaleY(yScale)(Math.max(d.Open, d.Close)))
+				   .attr("width", xBand.bandwidth()*d3.event.transform.k);
+			chartBody.selectAll(".stem").attr("x1", (d, i) => d3.event.transform.rescaleX(xScale)(i) - xBand.bandwidth()/2 + xBand.bandwidth()*0.5);
+			chartBody.selectAll(".stem").attr("x2", (d, i) => d3.event.transform.rescaleX(xScale)(i) - xBand.bandwidth()/2 + xBand.bandwidth()*0.5);
+			//chartBody.selectAll(".stem").attr("y1", (d, i) => d3.event.transform.rescaleY(yScale)(d.High));
+			//chartBody.selectAll(".stem").attr("y2", (d, i) => d3.event.transform.rescaleY(yScale)(d.Low));
 
-			//console.log(xScaleZ.domain())
-
-			var xAxisZ = d3.axisBottom()
-					   .scale(xScaleZ)
-					   .tickValues(xScaleZ.domain().filter(function(d,i){
-							scaleFloor = Math.floor(t.k)
-							//console.log(scaleFloor)
-							map = {1 : 300, 2 : 53.4, 3 : 14.1, 4 : 7.9, 5 : 5.6, 6 : 3.9, 7 : 2.9, 8 : 2.3}
-							//console.log(map[scaleFloor]);
-						return !(i%Math.floor(map[scaleFloor]/2))
-					}))
-					.tickFormat(function(d){
-						hours = d.getHours()
-						minutes = (d.getMinutes()<10?'0':'') + d.getMinutes() 
-						amPM = hours < 13 ? 'am' : 'pm'
-						return hours + ':' + minutes + amPM + ' ' + d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear()}
-				    );
-
-
-			d3.select('.x-axis').call(xAxisZ)
+			gX.selectAll(".tick text")
+			  .call(wrap, xBand.bandwidth())
 			
-			yScale.domain([minp, maxp]).range([h, 0]);
-			gY.call(yAxis.scale(d3.event.transform.rescaleY(yScale)));
-			
-			/* var invDomain = xScaleZ.domain()
-			var invRange = xScaleZ.range()
-			var invScale = d3.scaleQuantize().domain(invRange).range(invDomain)
+			//yScale.domain([ymin, ymax]).range([h, 0]);
+			//gY.call(yAxis.scale(t.rescaleY(yScale)));
 
-			leftDate = invScale(Math.abs(xScaleZ.range()[0])/t.k)
-			rightDate = invScale(w-Math.abs(w-xScaleZ.range()[1])/t.k) */
-
-			//console.log(leftDate, rightDate);
-
-			//gX.selectAll(".tick text")
-		  	//	.call(wrap, xScale.bandwidth())
-
-			//d3.zoom().on("start", console.log("Its started!"));
-			//d3.zoom().on("zoom", sleep(3000))
-			//d3.zoom().on("end", console.log(xAxis.tickValues()));
 		}
-		
+
+		let hideTicksWithoutLabel = function() {
+			d3.selectAll('.xAxis .tick text').each(function(d){
+			  if(this.innerHTML === '') {
+				this.parentNode.style.display = 'none'
+			  }
+			})
+		  }
 	});
-}
-
-function getShortMonth(date){
-	var month = date.getMonth()
-	months = {0 : 'Jan', 1 : 'Feb', 2 : 'Mar', 3 : 'Apr', 4 : 'May', 5 : 'Jun', 6 : 'Jul', 7 : 'Aug', 8 : 'Sep', 9 : 'Oct', 10 : 'Nov', 11 : 'Dec'}
-
-	if (month == 0 | month == 7){
-		return months[month] + ' ' + date.getFullYear()
-	}
-	else {
-		return months[month]
-	}
 }
 
 function wrap(text, width) {
@@ -255,16 +180,5 @@ function wrap(text, width) {
 	  }
 	});
 }
-
-
-
-function sleep(milliseconds) {
-	var start = new Date().getTime();
-	for (var i = 0; i < 1e7; i++) {
-	  if ((new Date().getTime() - start) > milliseconds){
-		break;
-	  }
-	}
-  }
 
 drawChart();
